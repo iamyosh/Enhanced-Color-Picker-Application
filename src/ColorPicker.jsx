91dcefb2833e133e356a0ac86e0e24d59bde1075
iamyosh <yoshedirisinghe28@gmail.com>
2025-04-18 02:48:33 +0530

    Initial commit

---
+++ src/ColorPicker.jsx
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react"
+import tinycolor from "tinycolor2";
+
+function ColorPicker(){
+
+    const [color, setColor] = useState("#FFFFFF");
+    const [color2, setColor2] = useState("#000000");
+    const [colorHistory, setColorHistory] = useState([]);
+    const [Favorites, setFavorites] = useState(() => JSON.parse(localStorage.getItem("Favorites")) || []);
+    const [darkMode, setDarkMode] = useState();
+    
+
+
+    const handleColorChange = (event) => setColor(event.target.value);
+
+    const copyToClipboard = () => navigator.clipboard.writeText(color);
+
+    const getContrastColor = (hex) => tinycolor(hex).isLight()? '#000' : '#fff';
+    const getColorName = (hex) => tinycolor(hex).toName() || 'Unknown';
+
+
+
+    return(
+        <div className="color-picker-container">
+            <h1>🎨 Enhanced Color Picker</h1>
+
+            <div className="color-display" style={{backgroundColor:color, color:getContrastColor(color)}} onClick={copyToClipboard} title="Click to copy HEX code">
+
+            <p>{color} | {tinycolor(color).toRgbString()} | {getColorName(color)}</p>
+            </div>
+
+            <label>Pick a Color: </label>
+            <input type="color" value={color} onChange={handleColorChange}></input>
+
+            <div>
+                <button onClick={generateRandomColor}>✨Random Color</button>
+                <button onClick={toggleFavorites}>{Favorites.includes(color)? '⭐Unfavorite' : '🌟Favorite'}</button>
+
+                <button onClick={setDarkMode(!darkMode)}>{darkMode? '🌞 Light Mode' : '🌙 Dark Mode'}</button>
+            </div>
+
+
+            
+        </div>
+
+    );
+
+}
+
+
+export default ColorPicker
