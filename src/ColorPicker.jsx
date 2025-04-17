aa5afcb730725ec0fae366646d3dde8e5f0df9aa
iamyosh <yoshedirisinghe28@gmail.com>
2025-04-18 02:01:53 +0530

    Add files via upload
    The main folder 'react-color-picker' contains the subfolder 'color-picker-application' which contains these files/folders

---
+++ src/ColorPicker.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from "react"
+import tinycolor from "tinycolor2";
+
+function ColorPicker(){
+
+    const [color, setColor] = useState("#FFFFFF");
+    const [color2, setColor2] = useState("#000000");
+    const [colorHistory, setColorHistory] = useState();
+    const [favColor, setFavColor] = useState();
+
+    
+
+
+
+
+
+}
+
+
+export default ColorPicker
